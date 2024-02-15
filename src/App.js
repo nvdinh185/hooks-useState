bb7e2856d56653933363ff0c17f4a0a893405890
nvdinh185 <nvdinh0766777123@gmail.com>
2024-02-15 14:14:13 +0700

    x

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function App() {
+  const [counter, setCounter] = useState(1);
+
+  const handleIncrease = () => {
+    setCounter(counter + 1);
+  }
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>{counter}</h1>
+      <button onClick={handleIncrease}>Increase</button>
+    </div>
+  );
+}
+
+export default App;
